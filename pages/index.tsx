6a6c7b4053cc2a3bdc3b9ff6dfd5269826fa66ca
Luis Martinez <luisgmp1997@gmail.com>
2021-12-10 15:10:12 -0400

    add title page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from "next";
-
+import Head from "next/head";
 const Home: NextPage = () => {
   return (
-    <div className="container">
-      <h1 className="title">Estamos en mantenimiento</h1>
-      <h2 className="subtitle">Pronto volveremos</h2>
-    </div>
+    <>
+      <Head>
+        <title>Garcia Tuñon</title>
+      </Head>
+      <div className="container">
+        <h1 className="title">Estamos en mantenimiento</h1>
+        <h2 className="subtitle">Pronto volveremos</h2>
+      </div>
+    </>
   );
 };
 
