0ff76a2c7dc87b7e129ad74190c42c7df7808188
Luis Martinez <luisgmp1997@gmail.com>
2022-01-12 19:43:37 -0400

    pagina de termino y condiciones agregada

---
+++ src/pages/terminos-condiciones-diagnostico.tsx
@@ -0,0 +1,142 @@
+import React from "react";
+import Head from "next/head";
+import Img from "next/image";
+
+const terminosCondicionesDiagnostico = () => {
+  return (
+    <>
+      <Head>
+        <title>Garcia Tuñon</title>
+      </Head>
+      <div className="container mx-auto">
+        <h3 className="text-2xl font-bold text-[#E7B864] mt-10 mb-3">
+          Enfoque
+        </h3>
+        <p className="mb-3 text-lg">
+          La empresa García-Tuñón Chacao C.A J-294888275, organiza el Concurso
+          “Cuéntanos qué Chevrolet tienes y gánate un diagnóstico de 25 puntos”.
+        </p>
+        <p className="mb-3 text-lg">
+          La dinámica se desarrollará a través de la plataforma de Instagram
+          únicamente (no Facebook) y el objetivo será recompensar a clientes
+          nuevos a través de un Diagnóstico de 25 puntos como premio. El
+          beneficio será exclusivo para los clientes nuevos de la marca. Los
+          concursantes deben estar ubicados dentro de Caracas y zonas aledañas.
+        </p>
+        <h3 className="text-2xl font-bold text-[#E7B864] mt-10 mb-5">
+          Finalidad
+        </h3>
+        <p className="mb-3 text-lg">
+          La finalidad de este concurso es recompensar a clientes nuevos a
+          través del otorgamiento de un Diagnóstico de 25 puntos como premio a
+          nivel de servicios.
+        </p>
+        <h3 className="text-2xl font-bold text-[#E7B864] mt-10 mb-5">
+          Periodo del concurso
+        </h3>
+        <p className="mb-3 text-lg">
+          Del 12 al 16 de enero del año 2022 (hasta las 8:00 pm del día Domingo
+          16 de enero)
+        </p>
+        <h3 className="text-2xl font-bold text-[#E7B864] mt-10 mb-5">
+          Requisitos para validación en la participación
+        </h3>
+        <p className="mb-3 text-lg">
+          Las personas que deseen participar deberán cumplir los siguientes
+          pasos:
+        </p>
+        <ul
+          className="list-disc list-inside "
+        >
+          <li>Seguir la cuenta de García-Tuñón en Instagram</li>
+          <li>Comentarnos cuál es el modelo del carro y el kilometraje</li>
+          <li>
+            Etiquetar al menos a dos usuarios (no cuentas de famosos ni de
+            empresas, solo personas naturales) y usar el Hashtag
+            #YoGanoconGarcíaTuñón
+          </li>
+        </ul>
+        <h3 className="text-2xl font-bold text-[#E7B864] mt-10 mb-5">
+          Condiciones de la promoción y el premio a otorgar
+        </h3>
+        <ul
+          className="list-disc list-inside "
+        >
+          <li>Las cuentas que participen deben ser reales.</li>
+          <li>El concurso es válido para Caracas y zonas aledañas.</li>
+          <li>El premio a otorgar será un diagnóstico de 25 pts.</li>
+          <li>Son 5 ganadores.</li>
+          <li>Se prohíbe etiquetar a famosos o cuentas falsas.</li>
+          <li>Válido únicamente para clientes nuevos.</li>
+          <li>
+            El ganador/a será anunciado el día Lunes (17/01/22) en la tarde a
+            las 3.30 p.m. Se usará una herramienta online de selección aleatoria
+            AppSorteos. Una vez seleccionado el ganador/a, luego de pasadas las
+            primeras 48 horas si no aparece, se procederá a seleccionar a otro
+            ganador de manera aleatoria.
+          </li>
+        </ul>
+        <h3 className="text-2xl font-bold text-[#E7B864] mt-10 mb-5">
+          Sobre los Participantes
+        </h3>
+        <ul
+          className="list-disc list-inside "
+        >
+          <li>
+            Concurso válido solo para clientes que no estén registrados en la
+            base de datos de García-Tuñón (Clientes Nuevos).
+          </li>
+          <li>Personas naturales ubicadas en Caracas y zonas aledañas.</li>
+          <li>
+            Se prohíbe la participación en el concurso de cualquier empleado o
+            persona involucrada con la organización.
+          </li>
+          <li>Válido solo para mayores de edad.</li>
+        </ul>
+        <h3 className="text-2xl font-bold text-[#E7B864] mt-10 mb-5">
+          Condiciones generales sobre la participación
+        </h3>
+        <p className="mb-3 text-lg">
+          La participación es gratuita y voluntaria.
+        </p>
+        <h3 className="text-2xl font-bold text-[#E7B864] mt-10 mb-5">
+          Sobre las descalificaciones y penalizaciones
+        </h3>
+        <p className="mb-3 text-lg">
+          La participación de los concursantes, así como la publicación de los
+          comentarios que estos realicen, no podrán vulnerar bajo ningún
+          concepto las reglas comunitarias de Instagram ni las Condiciones de
+          uso de Instagram.
+        </p>
+        <p className="mb-3 text-lg">
+          Si se evidenciase que cualquiera de los participantes no cumple con
+          los requisitos exigidos en las bases legales anteriormente expuestas,
+          o si los datos proporcionados para participar no fueran válidos, su
+          participación se considerará nula y quedarán automáticamente excluidos
+          del concurso perdiendo así cualquier derecho sobre los premios
+          otorgados.
+        </p>
+        <h3 className="text-2xl font-bold text-[#E7B864] mt-10 mb-5">
+          Sobre los comentarios y opiniones ajenas a la naturaleza del concurso
+        </h3>
+        <p className="mb-3 text-lg">
+          No se permitirán comentarios u opiniones cuyo contenido se considere
+          inadecuado, ofensivo, injurioso o discriminatorio. Tampoco se
+          permitirán comentarios contra un particular que vulneren los
+          principios de derecho al honor, a la intimidad personal, familiar y a
+          la propia imagen.
+        </p>
+        <h3 className="text-2xl font-bold text-[#E7B864] mt-10 mb-5">
+          Sobre la protección de datos
+        </h3>
+        <p className="mb-3 text-lg">
+          Una vez participando, cada concursante consiente que sus datos
+          personales sean incorporados a un fichero, titularidad de ésta, con la
+          finalidad de gestionar el sorteo, así como difundir y dar publicidad a
+          sus resultados, y tramitar la entrega del premio.
+        </p>
+      </div>
+    </>
+  );
+};
+export default terminosCondicionesDiagnostico;
