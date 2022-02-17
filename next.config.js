531c8e5d55d5b45e1d6fd70c61527e448a698f0e
Luis Martinez <luisgmp1997@gmail.com>
2022-02-17 10:53:05 -0400

    reactivacion de la pagina de cambio de aceites

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/cambioaceiteyfiltro",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/cambioaceiteyfiltro",
+  //       destination: "/",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
