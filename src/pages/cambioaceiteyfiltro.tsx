dee3d2c9af6a2323797070509e2b8ce9148bd235
Luis Martinez <luisgmp1997@gmail.com>
2022-02-17 13:57:10 -0400

    ajustes em texto

--- src/pages/cambioaceiteyfiltro.tsx
+++ src/pages/cambioaceiteyfiltro.tsx
@@ -146,7 +146,6 @@ const CambioAceite: NextPage = () => {
                 Aprovecha{" "}
                 <span className="text-[#FF3D00]"> hasta 15% de descuento</span>{" "}
                 Aprovecha 15% de descuento en Cambio de Aceite y Filtro con
-                nuestra Promoción Especial en Cambio de Aceite y Filtro con
                 nuestra Promoción Especial
               </h2>
               <p className="mb-3 text-lg text-center">
